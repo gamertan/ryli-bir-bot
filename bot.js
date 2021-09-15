9b1b7158ca7ab57d04df83199a8d25b87a0c4ce7
Cole Speelman <crspeelman@gmail.com>
2021-09-15 18:19:12 -0400

    remove traditional commands for slashcommands

--- bot.js
+++ bot.js
@@ -1,12 +1,47 @@
-require('dotenv').config();
-const discord = require('discord.js');
-const client = new discord.Client();
+const dotenv = require('dotenv').config();
+const fs = require('fs');
+const { Client, Collection, Intents, GuildMemberRoleManager } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS] });
+
 const prefix = process.env.PREFIX;
 
-client.login(process.env.BOT_TOKEN);
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.on('message', message => {
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+client.on('ready', () => {
+    console.log(`${client.user.tag} has logged in...`);
+});
+
+client.on('messageDelete', message => {
+    console.log(`A message was deleted: ${message}`);
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+client.on('messageCreate', message => {
     //Role selection channel
+    console.log(message);
+
     if (message.channel.id === '707334497414414407') {
         if (isNaN(message.content)) {
             message.author.send(`Your attempt to join a role failed. Please enter only the number of the channel you'd like to join in the text input.`);
@@ -36,6 +71,14 @@ client.on('message', message => {
                 });
             message.delete();
         } else if (message.content === '4') {
+            message.member.roles.add('840315068692693014')
+                .then(member => message.author.send("You will now receive MGP Gang notifications!"))
+                .catch(err => {
+                    console.log(err);
+                    message.author.send("Something went wrong when we tried to apply your role...");
+                });
+            message.delete();
+        } else if (message.content === '5') {
             message.member.roles.add('824788246886219796')
                 .then(member => message.author.send("You can now access World of Warcraft content."))
                 .catch(err => {
@@ -43,89 +86,9 @@ client.on('message', message => {
                     message.author.send("Something went wrong when we tried to apply your role...");
                 });
             message.delete();
-        } /*else if (message.content === '*') {
-            message.member.roles.add('707336844202868776')
-                .then(member => message.author.send("You can now access world news content."))
-                .catch(err => {
-                    console.log(err);
-                    message.author.send("Something went wrong when we tried to apply your role...");
-                });
-            message.delete();
-        }*/
-    }
-
-    // Commands
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const command = args.shift().toLowerCase();
-
-    if (command === `ping`){
-        message.channel.send('Pong!');
-    } else if (command === `beep`){
-        message.channel.send('Boop.');
-    } else if (command === `server`){
-        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-    } else if (command === `args-info`){
-        if (!args.length){
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-    } else if (command === `args-first`) {
-        if (!args.length) {
-            return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-        }
-        message.channel.send(`Command name: ${command}\nFirst argument: ${args[0]}`);
-    } else if (command === `dice` || command === `roll`) {
-        if (!args.length){
-            if (message.author.tag === "TexanJoe#6165") {
-                return message.reply("rolled 99");
-            }
-            message.reply("rolled " + dice());
-        } else {
-            args.forEach(name => message.channel.send(`${name} rolled ${dice()}.`))
-        }
-    } else if (command === `role`) {
-        if(!args.length){
-            return message.channel.send(`${message.member.
-                nickname}, you didn't provide a role...\nPlease use one of the following: \`worldnews\`, \`raiders\`, \`crafters\`.`);
-        } else {
-            if(args[0] === `raiders`){
-                message.member.roles.add('676489600361037824')
-                    .then(member => message.author.send("You can now access raid content."))
-                    .catch(err => {
-                        console.log(err);
-                        message.author.send("Something went wrong when we tried to apply your role...");
-                    });
-                    message.delete();
-            }
-            if(args[0] === `crafters`){
-                message.member.roles.add('709461046833971291')
-                    .then(member => message.author.send("You can now access crafters content."))
-                    .catch(err => {
-                        console.log(err);
-                        message.author.send("Something went wrong when we tried to apply your role...");
-                    });
-                    message.delete();
-            }
-            if (args[0] === `worldnews`) {
-                message.member.roles.add('707336844202868776')
-                    .then(member => message.author.send("You can now access world news content."))
-                    .catch(err => {
-                        console.log(err);
-                        message.author.send("Something went wrong when we tried to apply your role...");
-                    });
-                message.delete();
-            }
         }
     }
 
-
 });
 
-
-client.on('ready', () => {
-    console.log(`${client.user.tag} has logged in...`);
-});
-
-const dice = () => Math.floor(Math.random() * 100);
-
+client.login(process.env.BOT_TOKEN);
